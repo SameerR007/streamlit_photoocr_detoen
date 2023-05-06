b4bfe7fa355131b2bab1611097ad8dca109f74bb
SameerR007 <100122935+SameerR007@users.noreply.github.com>
2023-05-06 20:28:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
 import easyocr
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-from googletrans import Translator
 import streamlit as st
+import numpy as np
 from PIL import Image
-uploaded_image=st.file_uploader("Upload image",type=["jpg","png",])
-if(uploaded_image!=None):
-    display_image=Image.open(uploaded_image)
-    display_image=display_image.convert("RGB")
-    st.image(display_image)
-    if st.button("Translate"):
-        st.text("This may take a minute or two")
-        reader = easyocr.Reader(['de'])
-        result = reader.readtext(np.array(display_image))
-        img = np.array(display_image)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        for detection in result: 
-            try:
-                top_left = tuple(detection[0][0])
-                bottom_right = tuple(detection[0][2])
-                text = detection[1]
-                translator=Translator()
-                translation=translator.translate(text,dest="en")
-                #img = cv2.rectangle(img,top_left,bottom_right,(0,255,0),3)
-                img = cv2.putText(img,translation.text,top_left, font, 0.5,(255,0,0),1,cv2.LINE_AA)
-            except:
-                pass    
-        st.image(img)
-        st.text("Thank you for your patience")
+from googletrans import Translator
+import cv2
+
+def ocr_translation(image, language):
+    reader = easyocr.Reader([language])
+    result = reader.readtext(image)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for detection in result: 
+        try:
+            top_left = tuple(detection[0][0])
+            bottom_right = tuple(detection[0][2])
+            text = detection[1]
+            translator = Translator()
+            translation = translator.translate(text, dest="en")
+            image = cv2.putText(image, translation.text, top_left, font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+        except:
+            pass    
+    return image
+
+
+st.title("OCR Translation App")
+
+# Upload image
+image_file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
+if image_file is not None:
+    image = Image.open(image_file)
+    image=image.convert("RGB")
+    image = np.array(image)
+    image = ocr_translation(image, 'de')
+    st.image(image, caption='Translated image')
